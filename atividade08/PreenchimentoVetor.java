ed11358e11b5b6a721b3bbd79489f1ac612572b3
GiovannaLyssa99 <giovannalyssa@discente.ufg.br>
2024-04-26 14:10:36 -0300

    Melhorias em PreenchimentoVetor

--- atividade08/PreenchimentoVetor.java
+++ atividade08/PreenchimentoVetor.java
@@ -14,29 +14,32 @@ public class PreenchimentoVetor {
         try {
             while (true) {
                 System.out.print("Digite um número inteiro (0 para parar): ");
-                int numero = scanner.nextInt();
-                
-                if (numero == 0) {
+                try {
+                    int numero = scanner.nextInt();
+                    
+                    if (numero == 0) {
+                        vetor[index++] = numero;
+                        break;
+                    }
+                    
                     vetor[index++] = numero;
-                    break;
-                }
-                
-                vetor[index++] = numero;
-                
-                if (index == 10) {
-                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("Erro: Valor informado não é numérico. Ignorando valor...");
+                    scanner.next(); // Limpa o buffer do scanner
                 }
             }
             
             System.out.println("\nValores inseridos no vetor:");
-            for (int i = 0; i < index; i++) {
+            int imprimirQuantidade = Math.min(index, vetor.length);
+            for (int i = 0; i < imprimirQuantidade; i++) {
                 System.out.println(vetor[i]);
             }
             
-        } catch (InputMismatchException e) {
-            System.out.println("Erro: Valor informado não é numérico.");
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Erro: Número máximo de valores excedido.");
+            System.out.println("Erro: Número máximo de valores excedido. Imprimindo os 10 primeiros valores:");
+            for (int i = 0; i < vetor.length; i++) {
+                System.out.println(vetor[i]);
+            }
         } finally {
             scanner.close();
         }
